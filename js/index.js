20533dbab33b2a1501744a885f796ae4ebc73977
MariamYasser227 <yassermariam444@gmail.com>
2025-09-16 21:35:54 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,26 +1,130 @@
-let div = document.querySelector("#Main");
-let photoDiv = document.querySelector("#photoDiv");
-let input = document.querySelector("#Input");
+const countries = {
+  "am-ET": "Amharic",
+  "ar-SA": "Arabic",
+  "be-BY": "Bielarus",
+  "bem-ZM": "Bemba",
+  "bi-VU": "Bislama",
+  "bjs-BB": "Bajan",
+  "bn-IN": "Bengali",
+  "bo-CN": "Tibetan",
+  "br-FR": "Breton",
+  "bs-BA": "Bosnian",
+  "ca-ES": "Catalan",
+  "cop-EG": "Coptic",
+  "cs-CZ": "Czech",
+  "cy-GB": "Welsh",
+  "da-DK": "Danish",
+  "dz-BT": "Dzongkha",
+  "de-DE": "German",
+  "dv-MV": "Maldivian",
+  "el-GR": "Greek",
+  "en-GB": "English",
+  "es-ES": "Spanish",
+  "et-EE": "Estonian",
+  "eu-ES": "Basque",
+  "fa-IR": "Persian",
+  "fi-FI": "Finnish",
+  "fn-FNG": "Fanagalo",
+  "fo-FO": "Faroese",
+  "fr-FR": "French",
+  "gl-ES": "Galician",
+  "gu-IN": "Gujarati",
+  "ha-NE": "Hausa",
+  "he-IL": "Hebrew",
+  "hi-IN": "Hindi",
+  "hr-HR": "Croatian",
+  "hu-HU": "Hungarian",
+  "id-ID": "Indonesian",
+  "is-IS": "Icelandic",
+  "it-IT": "Italian",
+  "ja-JP": "Japanese",
+  "kk-KZ": "Kazakh",
+  "km-KM": "Khmer",
+  "kn-IN": "Kannada",
+  "ko-KR": "Korean",
+  "ku-TR": "Kurdish",
+  "ky-KG": "Kyrgyz",
+  "la-VA": "Latin",
+  "lo-LA": "Lao",
+  "lv-LV": "Latvian",
+  "men-SL": "Mende",
+  "mg-MG": "Malagasy",
+  "mi-NZ": "Maori",
+  "ms-MY": "Malay",
+  "mt-MT": "Maltese",
+  "my-MM": "Burmese",
+  "ne-NP": "Nepali",
+  "niu-NU": "Niuean",
+  "nl-NL": "Dutch",
+  "no-NO": "Norwegian",
+  "ny-MW": "Nyanja",
+  "ur-PK": "Pakistani",
+  "pau-PW": "Palauan",
+  "pa-IN": "Panjabi",
+  "ps-PK": "Pashto",
+  "pis-SB": "Pijin",
+  "pl-PL": "Polish",
+  "pt-PT": "Portuguese",
+  "rn-BI": "Kirundi",
+  "ro-RO": "Romanian",
+  "ru-RU": "Russian",
+  "sg-CF": "Sango",
+  "si-LK": "Sinhala",
+  "sk-SK": "Slovak",
+  "sm-WS": "Samoan",
+  "sn-ZW": "Shona",
+  "so-SO": "Somali",
+  "sq-AL": "Albanian",
+  "sr-RS": "Serbian",
+  "sv-SE": "Swedish",
+  "sw-SZ": "Swahili",
+  "ta-LK": "Tamil",
+  "te-IN": "Telugu",
+  "tet-TL": "Tetum",
+  "tg-TJ": "Tajik",
+  "th-TH": "Thai",
+  "ti-TI": "Tigrinya",
+  "tk-TM": "Turkmen",
+  "tl-PH": "Tagalog",
+  "tn-BW": "Tswana",
+  "to-TO": "Tongan",
+  "tr-TR": "Turkish",
+  "uk-UA": "Ukrainian",
+  "uz-UZ": "Uzbek",
+  "vi-VN": "Vietnamese",
+  "wo-SN": "Wolof",
+  "xh-ZA": "Xhosa",
+  "yi-YD": "Yiddish",
+  "zu-ZA": "Zulu",
+};
+let firstLang = document.querySelector("#firstLang");
+let secondLang = document.querySelector("#secondLang");
+let selectFirst = document.querySelector("#selectFirst");
+let selectSecond = document.querySelector("#selectSecond");
 let btn = document.querySelector("#btn");
 
-fetch("https://api.imgflip.com/get_memes")
-  .then((response) => response.json())
-  .then((result) => {
-    btn.addEventListener("click", () => {
-      let memes = result.data.memes;
-      let index = +input.value;
-      if (index >= memes.length || input.value === "" || isNaN(index)) {
-        photoDiv.innerHTML = `<h1 class="text-danger"> Please Enter Valid Number </h1>`;
-      } else {
-        let selectedMeme = memes[index];
-        let imageOfMeme = selectedMeme.url;
-        let userName = selectedMeme.name;
-        photoDiv.innerHTML = `<h1 class="text-danger"> ${userName} </h1>
-        <img class="img-fluid w-50" src="${imageOfMeme}" />`;
-      }
-      input.value = "";
+Object.keys(countries).forEach((key) => {
+  let option = `<option value=${key}> ${countries[key]} </option>`;
+  selectFirst.innerHTML += option;
+  selectSecond.innerHTML += option;
+});
+selectFirst.value = "en-GB";
+selectSecond.value = "ar-SA";
+
+btn.addEventListener("click", () => {
+  let from = firstLang.value;
+  let fromLang = selectFirst.value;
+  let toLang = selectSecond.value;
+  fetch(
+    `https://api.mymemory.translated.net/get?q=${from}&langpair=${fromLang}|${toLang}`
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      let translatedText = result.responseData.translatedText;
+      secondLang.value = translatedText;
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+});
